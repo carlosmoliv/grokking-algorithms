dc225fea0f00ce22f38d88dcd3bdaf382cf2819d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-05-15 15:59:49 -0300

    add quicksort sample

---
+++ src/chapter-4-quicksort/quick-sort.spec.ts
@@ -0,0 +1,31 @@
+const quickSort = (list: number[]): number[] => {
+  // Base case: arrays with 0 or 1 element are already “sorted.”
+  if (list.length < 2) {
+    return list;
+  } else {
+    // Recursive case
+    const pivot = list[0];
+    const less = [];
+    const greater = [];
+
+    for (let i = 1; i < list.length; i++) {
+      if (list[i] <= pivot) {
+        // Sub array of elements less than the pivot
+        less.push(list[i]);
+      } else {
+        // Sub array of elements greater than the pivot
+        greater.push(list[i]);
+      }
+    }
+
+    return [...quickSort(less), pivot, ...quickSort(greater)];
+  }
+};
+
+describe("quickSort", () => {
+  it("", async () => {
+    const result = quickSort([10, 5, 2, 3]);
+
+    expect(result).toEqual([2, 3, 5, 10]);
+  });
+});
